c90b4bfd449fd57b2e698168cb51e573da25f041
Orlando Avila <orlando.avilag@hotmail.com>
2024-08-31 00:21:16 -0600

    MiniTourist project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { MoreAboutMtComponent } from './more-about-mt/more-about-mt.component';
 import { WhereIsMtComponent } from './where-is-mt/where-is-mt.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'about-mt', component: AboutMtCardsComponent },
